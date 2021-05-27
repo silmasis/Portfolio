176fb5dc75a04bcc563baa7757ea81b3cdb3ef6e
Silavong <silmasis@gmail.com>
2021-05-27 10:38:40 -0400

    made intro page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,39 @@
 import './Intro.scss'
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import { init } from 'ityped';
+import { useEffect, useRef } from 'react';
 
 export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed: 50,
+            strings: ["Web Developer", "Freelance Musician"]
+        });
+    }, [])
+
     return (
         <div className="intro" id='intro'>
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/headshot.png" alt="Me" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hello There, I'm</h2>
+                    <h1>Silavong</h1>
+                    <h3><span ref={textRef}></span></h3>
+                </div>
+                <a href="#about">
+                    <KeyboardArrowDownIcon className='icon' />
+                </a>
+                
+            </div>
         
         </div>
     )
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,7 +3,20 @@ import './Projects.scss'
 export default function Projects() {
     return (
         <div className='projects' id='projects'>
-            
+            <h1>Projects</h1>
+            <ul>
+                <li className='active'>Featured</li>
+                <li>Squawker</li>
+                <li>Calorie Counter</li>
+                <li>My Virtual Pantry</li>
+                <li>Brass Band of Central Florida</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="https://picsum.photos/200" alt="" />
+                    <h3>Squawker</h3>
+                </div>
+            </div>
         </div>
     )
 }
